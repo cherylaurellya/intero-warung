0f21f2e9b84dc96c9ab19f029e8de3b346d3a4cb
cherylaurellya <cherylaurellya577@gmail.com>
2025-12-02 15:00:27 +0700

    warung

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const dataWarung = [
+    {
+        "kd_produk": "A001",
+        "nm_brg": "kopi Bubuk 100g",
+        "hrg": "15000",
+        "ket_stok": "ada"
+    },
+    {
+        "kd_produk": "A002",
+        "nm_brg": "Gula Pasir 1kg",
+        "hrg": "13500",
+        "ket_stok": "habis"
+    },
+    {
+        "kd_produk": "A003",
+        "nm_brg": "Minyal Goreng 1L",
+        "hrg": "14000",
+        "ket_stok": "ada"
+    }
+
+];
+
+app.get('/', (req, res) => {
+    res.send('Selamat Datang di Warung!');
+});
+
+app.get('/api/warung', (req, res) => {
+    res.json(dataWarung);
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log(`Server jalan di http://localhost:${port}`);
+});
+
+module.exports = app;
